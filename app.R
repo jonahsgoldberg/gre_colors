a9f70b4a23dfa57e1b2d4397b880c5a5b6bfcaa8
jonahsgoldberg <62079513+jonahsgoldberg@users.noreply.github.com>
2022-12-17 21:44:26 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,16 +1,18 @@
-setwd("~/Documents/R/gre_color")
+#setwd("~/Documents/R/gre_color")
 library(tidyverse)
 library(npreg)
 library(truncnorm)
 library(pals)
 library(oompaBase)
 library(shiny)
-library(rsconnect)
+#library(rsconnect)
 
 # Define the user interface for the Shiny app
 ui <- fluidPage(
   # Add a text input for the GRE score
-  textInput(inputId = "gre_score", label = "GRE Score:", value = "150"),
+  textInput(inputId = "gre_score", label = "GRE Score:"),
+  # Add a checkbox to toggle the use of the noised score
+  checkboxInput(inputId = "use_noise", label = "Add Noise"),
   # Add radio buttons to select the color palette
   radioButtons(inputId = "palette", label = "Color Palette:", 
                choices = c("Red to Blue" = "warmcool", "Dark Green to Light Green" = "greenscale", "Blue to Yellow" = "parula"),
@@ -23,13 +25,17 @@ ui <- fluidPage(
 server <- function(input, output) {
   # Reactive function to generate the gre_color plot
   gre_color_plot <- reactive({
-    # Calculate the noised score using the input GRE score
-    noised_score <- as.numeric(input$gre_score) + rtruncnorm(mean=0, sd=1, n = 1, a = -3, b = 3)
+    # Calculate the noised score if the "use_noise" checkbox is checked, otherwise use the input GRE score
+    if (input$use_noise) {
+      score <- as.numeric(input$gre_score) + rtruncnorm(mean=0, sd=1, n = 1, a = -3, b = 3)
+    } else {
+      score <- as.numeric(input$gre_score)
+    }
     
-    # Generate the color palette and map the noised score to a color
+    # Generate the color palette and map the score to a color
     palette_function <- get(input$palette)
     palette <- palette_function(50)
-    col_num <- number2color(noised_score, colors = palette, ncol = 50, equidistant = TRUE, xmin = min(127), xmax = max(173))
+    col_num <- number2color(score, colors = palette, ncol = 50, equidistant = TRUE, xmin = min(127), xmax = max(173))
     
     # Create the data frame with x, y, and z variables set to 0
     x <- 0
